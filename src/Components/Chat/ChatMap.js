b9978f8a922a608ca070d4ba3f3815f9d09fd718
snoringarvind <snoringarvind@gmail.com>
2021-03-06 20:51:51 +0530

    date bug in chatlist

--- src/Components/Chat/ChatMap.js
+++ src/Components/Chat/ChatMap.js
@@ -32,19 +32,16 @@ const ChatMap = ({ value, index }) => {
                 </div>
                 <div className="time">
                   <span>
-                    {d.getHours() > 12
-                      ? d.getHours() - 12
-                      : d.getHours() === 0
-                      ? 12
-                      : d.getHours()}
-                    :
+                    {d.getHours() > 12 && d.getHours() - 12}
+                    {d.getHours() <= 12 && d.getHours() !== 0 && d.getHours()}
+                    {d.getHours() === 0 && "0" + d.getHours()}
                   </span>
                   <span>
                     {d.getMinutes() < 10
                       ? "0" + d.getMinutes()
                       : d.getMinutes()}
                   </span>
-                  <span>{d.getHours() > 12 ? "pm" : "am"}</span>
+                  <span>{d.getHours() >= 12 ? "pm" : "am"}</span>
                 </div>
               </div>
             </div>
